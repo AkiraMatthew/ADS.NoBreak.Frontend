b925b4477bb99d536846cae834614be21a87de82
Mateus Henrique <akira.dev.math@gmail.com>
2025-09-29 02:10:11 -0300

    dashboard needs review

--- src/app/pages/auth/login.ts
+++ src/app/pages/auth/login.ts
@@ -14,7 +14,10 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
     imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule, AppFloatingConfigurator],
     template: `
         <app-floating-configurator />
-        <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-screen overflow-hidden">
+        <div class="bg-surface-50 dark:bg-surface-950 flex flex-col items-center justify-center min-h-screen min-w-screen overflow-hidden">
+            <div class="flex flex-col pb-5">
+                <button pButton pRipple label="Back Home" routerLink="/" [rounded]="true"></button>
+            </div>
             <div class="flex flex-col items-center justify-center">
                 <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
                     <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
